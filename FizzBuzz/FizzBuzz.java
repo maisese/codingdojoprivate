df02970879f8703716faedc0ff0309d4b086a836
maisese <37495008+maisese@users.noreply.github.com>
2019-04-02 01:43:00 -0700

    Add files via upload

---
+++ FizzBuzz/FizzBuzz.java
@@ -0,0 +1,18 @@
+public class FizzBuzz {
+    public void happen(int number) {
+
+        if (number == 3) {
+            System.out.println("Fizz");
+        }
+        if (number == 5) {
+            System.out.println("Buzz");
+        }
+        if (number == 15) {
+            System.out.println("Fizz Buzz");
+        }
+        if (number == 2) {
+            System.out.println("2");
+        }
+
+    }
+}
---
+++ FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+public class FizzBuzzTest{
+    
+    public static void main(String[] args) {
+        FizzBuzz bubbles = new FizzBuzz();
+
+        bubbles.happen(3);
+        bubbles.happen(5);
+        bubbles.happen(15);
+        bubbles.happen(2);
+    }
+}
