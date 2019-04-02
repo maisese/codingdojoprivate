bede8f15d781b6ac3be49ca32763bf82a560d490
maisese <37495008+maisese@users.noreply.github.com>
2019-04-01 23:43:56 -0700

    Add files via upload

---
+++ javafun/MyInfo.java
@@ -0,0 +1,7 @@
+public class MyInfo {
+    public static void main(String[] args) {
+        System.out.println("My name is Mai");
+        System.out.println("I am 35 years old");
+        System.out.println("My hometown is San Leandro, CA");
+    }
+}
